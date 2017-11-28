66a22873bd06ce46d7c4ef896dafa2a31013048f
Juan Carlos Garcia Pelaez <juancarlosgpelaez@gmail.com>
2017-11-28 18:14:54 +0100

    test

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,14 +1,10 @@
-//vars/sendNotifications.groovy
+// vars/sendNotifications.groovy
 #!/usr/bin/env groovy
-
-
 
 
 /**
  * Send notifications based on build status string
  */
 def call(String buildStatus = 'STARTED', String channel, String credentialsGroup, String message) {
-colorCodeYellow = '#FFFF00'
-colorCodeGreen = '#00FF00'
-colorCodeRed = '#FF0000'
+
 }

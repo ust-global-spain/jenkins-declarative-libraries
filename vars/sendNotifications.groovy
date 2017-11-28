cc2a8b3c93a446cb3210b228437f5cb3069f2ed3
Juan Carlos Garcia Pelaez <juancarlosgpelaez@gmail.com>
2017-11-28 18:33:18 +0100

    add parameters

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,7 +1,7 @@
 /**
  * Send notifications based on build status string
  */
-def call(String buildStatus = 'STARTED') {
+def call(String buildStatus = 'STARTED', String channel, String credentialsGroup, String message) {
 	// build status of null means successful
 	buildStatus = buildStatus ?: 'SUCCESS'
 

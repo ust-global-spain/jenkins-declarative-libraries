e20cfe3941767385e4e93e7cdc6f67964496dd6f
Juan Carlos Garcia Pelaez <juancarlosgpelaez@gmail.com>
2017-11-28 18:36:36 +0100

    add method

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -5,5 +5,26 @@ def call(String buildStatus = 'STARTED', String channel, String credentialsGroup
 	// build status of null means successful
 	buildStatus = buildStatus ?: 'SUCCESS'
 
+	colorCodeYellow = '#FFFF00'
+	colorCodeGreen = '#00FF00'
+	colorCodeRed = '#FF0000'
 
+	// Default values
+	def colorCode = colorCodeGreen
+	def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
+	def summary = "${subject} (${env.BUILD_URL})"
+	def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+	  <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""" + message
+	
+	// Override default values based on build status
+	if (buildStatus == 'STARTED') {
+	  colorCode = colorCodeYellow
+	} else if (buildStatus == 'QUESTION') {
+	  colorCode = colorCodeYellow
+	} else if (buildStatus == 'SUCCESS') {
+	  colorCode = colorCodeGreen
+	} else {
+	  colorCode = colorCodeGreen
+	}
+	echo colorCode
 }

6ed38ea01adb971a9df40550246fdd58a4389e5d
Juan Carlos Garcia Pelaez <juancarlosgpelaez@gmail.com>
2017-11-28 18:32:00 +0100

    test

--- vars/sendNotifications.groovy
+++ vars/sendNotifications.groovy
@@ -1,12 +1,9 @@
-vars/sendNotifications.groovy
-#!/usr/bin/env groovy
-
 /**
  * Send notifications based on build status string
  */
 def call(String buildStatus = 'STARTED') {
-  // build status of null means successful
-  buildStatus = buildStatus ?: 'SUCCESS'
+	// build status of null means successful
+	buildStatus = buildStatus ?: 'SUCCESS'
 
 
 }

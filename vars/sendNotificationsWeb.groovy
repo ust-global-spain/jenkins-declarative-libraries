b2266aa3e39dda4953105bf888d1a1828ef44bb8
JoniSanchez <jonisanchezparedes@gmail.com>
2018-08-07 16:42:06 +0200

    delete color and library found.

--- vars/sendNotificationsWeb.groovy
+++ vars/sendNotificationsWeb.groovy
@@ -8,39 +8,23 @@ import groovyx.net.http.HTTPBuilder
 
 def call(String buildStatus = 'STARTED', String channel, String credentialsGroup, String message) {
   // build status of null means successful
-  /*buildStatus = buildStatus ?: 'SUCCESS'
+  buildStatus = buildStatus ?: 'SUCCESS'
 
-  colorCodeYellow = '#FFFF00'
-  colorCodeGreen = '#00FF00'
-  colorCodeRed = '#FF0000'*/
 
   // Default values
-  /*def colorCode = colorCodeGreen
   def subject = "${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
   def summary = "${subject} (${env.BUILD_URL})"
   def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
     <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>""" + message
   def detailsCisco = "\n" + message
     
-  if (buildStatus == 'STARTED') {
-    colorCode = colorCodeYellow
-  } else if (buildStatus == 'QUESTION') {
-    colorCode = colorCodeYellow
-  } else if (buildStatus == 'SUCCESS') {
-    colorCode = colorCodeGreen
-  } else if (buildStatus == 'FAILURE') {
-    colorCode = colorCodeRed
-    detailsCisco += "\n@channel"
-  } else {
-    colorCode = colorCodeGreen
-  }*/
+
   
   // Send notifications
-  //sendWebexTeamsMessage(channel, credentialsGroup, summary + detailsCisco, colorCode)
-  sendWebexTeamsMessage(channel, credentialsGroup, "test", "test")
+  sendWebexTeamsMessage(channel, credentialsGroup, summary + detailsCisco)
 }
 
-def sendWebexTeamsMessage(channel, credentialsGroup, message, color) {
+def sendWebexTeamsMessage(channel, credentialsGroup, message) {
   credentialsId = 'webexteams-' + credentialsGroup+ '-' + channel
   echo "CredentialsId:" + credentialsId
   echo "Webex Teams channel:" + channel
